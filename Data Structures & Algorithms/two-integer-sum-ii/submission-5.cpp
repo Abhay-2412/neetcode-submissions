a5d2379e9ee3488f3396d71c06aac7b8ca40ff98
Abhay <86143871+Abhay-2412@users.noreply.github.com>
2026-08-04 22:38:13 +0530

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) 
+    {
+	int left = 0;
+	int right = arr.size()-1;
+
+	while(left<right)
+	{
+		if(arr[left] + arr[right] < target)
+		{
+			left++;
+		}
+		else if(arr[left] + arr[right] > target)
+		{
+			right--;
+		}
+		else
+		{
+			return{left+1,right+1};
+		}
+
+	}
+	
+	return {};
+}
+};
