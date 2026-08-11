8b30bc00f9760ef552c7a2a4db2c4311354998a2
Abhay <86143871+Abhay-2412@users.noreply.github.com>
2026-08-11 16:57:39 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int freq1[256] = {0};
+        int freq2[256] = {0};
+        for (int i = 0; i < t.length(); i++) freq1[t[i]]++;
+
+        int toMatches = t.length();
+        int Matches = 0;
+        int left = 0, right = 0;
+        int minLen = INT_MAX;
+        int minStart = 0;              // <-- store index, not a string
+
+        int i = 0;
+        while (i < s.length()) {
+            while (Matches < toMatches && i < s.length()) {
+                freq2[s[i]]++;
+                if (freq2[s[i]] <= freq1[s[i]]) Matches++;
+                i++;
+            }
+            right = i;
+
+            while (Matches == toMatches) {
+                int len = right - left;
+                if (len < minLen) {
+                    minLen = len;
+                    minStart = left;   // <-- cheap: just an int assignment
+                }
+
+                if (freq2[s[left]] <= freq1[s[left]]) Matches--;
+                freq2[s[left]]--;
+                left++;
+            }
+        }
+
+        return minLen == INT_MAX ? "" : s.substr(minStart, minLen);  
+    }
+};
