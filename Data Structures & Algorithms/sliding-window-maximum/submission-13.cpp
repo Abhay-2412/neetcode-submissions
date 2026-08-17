8fd51dc329a253c940921fc92171790374216ab5
Abhay <86143871+Abhay-2412@users.noreply.github.com>
2026-08-17 15:09:00 +0530

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    
+    deque<int> window;
+    vector<int> res;
+    window.push_back(0);
+
+    for(int i=1; i<k; i++)
+    {
+    	while(!window.empty() && nums[window.back()] < nums[i] )
+    	{
+		    window.pop_back();
+	    }
+	
+	window.push_back(i);
+    }
+    int left = 1;
+    for(int right = k; right<nums.size(); right++)
+    {
+		res.push_back(nums[window.front()]);
+		
+		if(left > window.front())
+		{
+			window.pop_front();
+		}
+		
+			while(!window.empty() && nums[window.back()] < nums[right])
+			{
+				window.pop_back();
+			}
+			window.push_back(right);	
+			left++;
+    }
+    res.push_back(nums[window.front()]);   
+    return res;
+        
+    }
+};
